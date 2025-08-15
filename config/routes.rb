3c0c3824fb9216a3a031da3e85a00704daa0f807
Aurelien Miette <aurelien.miette@gmail.com>
2025-08-15 16:32:43 +0200

    front

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "lists#index"
 
-  resources :lists, except: [:index] do
+  resources :lists do
     resources :bookmarks
   end
 end
-
